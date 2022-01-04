40c334821668f907fe0ae0801f7ac16d7c00ce42
Richard Clarke <freshnewrich@gmail.com>
2022-01-03 22:04:24 -0800

    class work and js homework exercise for first day of js

---
+++ script.js
@@ -0,0 +1,115 @@
+// class notes
+
+    console.log("hello world");
+
+    var string1 = "Show me"
+    var string2 = " the moneyyyyy!"
+
+    var SHOWMETHEMONEY = string1 + string2
+
+    console.log(SHOWMETHEMONEY)
+
+
+
+// CLASS EXERCISE 
+
+    // This project is to be done in groups of 2, preferable no more than 3 people. The goal is to learn
+    // how to use variables in javascript. Each of the following results should be outputted to the
+    // console.
+    // 1. Define a variable that is an integer, and output the result to the console.
+    var beholdAnInteger = 1 
+    console.log(beholdAnInteger)
+
+    // 2. Define a variable that is a double/float, and output to the console.
+    var beholdADoubleFloat = 1.5
+    console.log(beholdADoubleFloat)
+
+    // 3. Define a string and output the result to the console.
+    var beholdAString = "I am string"
+
+    // 4. Define one number that is integer that is a float and one number that is a double/float.
+    var beholdAnIntegerFloat = 2.0
+    var beholdAnotherDoubleFloat = 2.5
+    console.log(beholdAnIntegerFloat)
+    console.log(beholdAnotherDoubleFloat)
+
+    // Write operations that will:
+    // a. Add the numbers
+    console.log(beholdAnInteger + beholdAnotherDoubleFloat)
+
+    // b. Divide the numbers
+    console.log(beholdAnIntegerFloat / beholdAnInteger)
+
+    // c. Multiply the numbers
+    console.log(beholdADoubleFloat * beholdAnIntegerFloat
+        )
+    // d. Subtract the numbers
+    console.log(beholdAnIntegerFloat - beholdADoubleFloat)
+
+    // e. Find the remainder
+    console.log(beholdAnIntegerFloat % beholdAnInteger)
+
+    // 5. Create two variables that are strings. Combine the strings to a third variable and write
+    // them to the console.
+    var string1 = "hip to the hop"
+    var string2 = " and you don't stop"
+    console.log(string1 + string2)
+
+    // 6. Create two variables, one that is a string and one that is an integer. Combine them and
+    // write the results to the console.
+    var string3 = "It goes to "
+    var spinalTap = 11
+    console.log(string3 + spinalTap)
+
+
+
+
+
+// H O M E W O R K
+
+    // Instructions/Exercises
+    // 1. Arithmetic Operation Exercises
+    // a. Add
+    // i. Create two variables each storing number. Add them together to get the
+    // sum of the two numbers
+    var homeworkVariable1 = 10
+    var homeworkVariable2 = 20
+    console.log(homeworkVariable1 + homeworkVariable2) // = 30
+
+    // b. Subtract
+    // i. Create two variables each storing number. Subtract them together to get
+    // the difference of the two numbers
+    var homeworkVariable3 = 30
+    var homeworkVariable4 = 29
+    console.log(homeworkVariable3 - homeworkVariable4)  // = 1
+
+    // c. Divide
+    // i. Create two variables each storing number. Divide them to get the quotient
+    // of the two numbers.
+    var homeworkVariable5 = 20
+    var homeworkVariable6 = 10
+    console.log(homeworkVariable5 / homeworkVariable6) // = 2
+
+    // d. Multiplication
+    // i. Create two variables each storing number. Multiple them together to get
+    // the product of the two numbers
+    var homeworkVariable7 = 10
+    var homeworkVariable8 = 20
+    console.log(homeworkVariable7 * homeworkVariable8) // = 200
+
+    // 2. String Concatenation
+    // a. Take two strings, “Hello” and “World” store each in their own variable and
+    // concatenate the two strings together. The result should be “Hello World”. Store
+    // the result in a new variable.
+    var hello = "Hello"
+    var world = "World"
+    helloWorld = hello + " " + world
+    console.log(helloWorld)
+
+    // b. Create the following string “My favorite food is ____”. The blank will be filled in
+    // with your favorite food. Your favorite food, the string “My favorite food is”, and the
+    // final string need to each be stored in their own variable. The final result should
+    // return something like “My favorite food is dinosaur chicken nuggets”
+    var fav = "My favorite food is "
+    var food = "coffee."
+    console.log(fav + food)
